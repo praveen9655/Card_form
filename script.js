92aac051434ccef16203e90bde32deba6f7b5957
Praveen K <118113479+praveen9655@users.noreply.github.com>
2023-07-27 06:38:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,108 +1,106 @@
+const form = document.querySelector('#form');
+const username = document.querySelector('#username');
+const card_num = document.querySelector('#card_nu');
+const cardMm = document.querySelector('#cardMm');
+const cardYy = document.querySelector('#cardYy');
+const cardCvc = document.querySelector('#cardCvc');
+//card display
 let carNum = document.getElementById('card_num');
 let carName = document.getElementById('card_name');
 let carMm = document.getElementById('card_mm');
 let carYy = document.getElementById('card_yy');
 let carCvc = document.getElementById('card_cvc');
-let carNumIn = document.getElementById('inCard_num');
-let carNameIn = document.getElementById('inCard_name');
-let carMmIn = document.getElementById('inCard_mm');
-let carYyIn = document.getElementById('inCard_yy');
-let carCvcIn = document.getElementById('inCard_cvc');
-let foRm = document.querySelector('.main_form');
-let compLete = document.querySelector('.comp');
-let myForm = document.getElementById('myForm');
-let wrna = document.querySelector('#wrnName');
-let wrnu = document.querySelector('#wrnNum');
-let wrnMm = document.querySelector('#wrnMm');
-let wrnCvc = document.querySelector('#wrncvc');
 
-myForm.addEventListener('click',(e) => {
-    e.preventDefault();
+const regex_char = /^[a-zA-Z ]+$/;
+const regex_num = /^[0-9]+$/;
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  validateInputs();
 });
 
-function liveUpinfo(){
-    carNameIn.addEventListener('input',function (){
-        
-        const regex = /^[a-zA-Z ]+$/;
-        if (regex.test(carNameIn.value) && carNameIn.value.length <= 20) {
-            carName.innerHTML = carNameIn.value;
-            carNameIn.classList.remove('shake-input');
-            carNameIn.style = `
-            border: 1px solid #220930; `
-            wrna.innerHTML=' ';
 
-        } else {
-            carNameIn.classList.add('shake-input');
-            carNameIn.style = `
-            border: 1px solid red; `
-            wrna.innerHTML='Wrong format';
-        }
-    });
-    carNumIn.addEventListener('input',function (){
-        const regex = /^[0-9]+$/;
-        if (regex.test(carNumIn.value) && carNumIn.value.length <= 16) {
-            carNum.innerHTML = carNumIn.value.replace(/(\d{4})/g, '$1 ');
-            carNumIn.classList.remove('shake-input');
-            carNumIn.style = `
-            border: 1px solid #220930; `
-            wrnu.innerHTML=' '; 
-        } else {
-            carNumIn.classList.add('shake-input');
-            carNumIn.style = `
-            border: 1px solid red; ` 
-            wrnu.innerHTML='Wrong format';
-        }
-    });
-    carYyIn.addEventListener('input',function (){
-        const regex = /^[0-9]+$/;
-        if (regex.test(carYyIn.value) && carYyIn.value.length <= 2) {
-            carYy.innerHTML = carYyIn.value;
-            carYyIn.classList.remove('shake-input');
-            carYyIn.style = `
-            border: 1px solid #220930; `
-            wrnMm.innerHTML=' '; 
-        } else {
-            carYyIn.classList.add('shake-input');
-            carYyIn.style = `
-            border: 1px solid red; ` 
-            wrnMm.innerHTML='Wrong format';
-        }
-    });
-    carMmIn.addEventListener('input',function (){
-        const regex = /^[0-9]+$/;
-        if (regex.test(carMmIn.value) && carMmIn.value.length <= 2) {
-            carMm.innerHTML = carMmIn.value;
-            carMmIn.classList.remove('shake-input');
-            carMmIn.style = `
-            border: 1px solid #220930; ` 
-            wrnMm.innerHTML=' '; 
+function liveUpinfo(input, regex, maxLength, display1) {
+    input.addEventListener('input', function () {
+        const value = input.value.trim();
 
-        } else {
-            carMmIn.classList.add('shake-input');
-            carMmIn.style = `
-            border: 1px solid red; ` 
-            wrnMm.innerHTML='Wrong format';
+        input.classList.remove('success', 'error');
+        setSuccess(input);
+
+        if (value.length === 0) {
+          setError(input, 'Field is empty');
         }
-    });
-    carCvcIn.addEventListener('input',function (){
-        const regex = /^[0-9]+$/;
-        if (regex.test(carCvcIn.value) && carCvcIn.value.length <= 3) {
-            carCvc.innerHTML = carCvcIn.value;
-            carCvcIn.classList.remove('shake-input');
-            carCvcIn.style = `
-            border: 1px solid #220930; `
-            wrnCvc.innerHTML=' ';
- 
-        } else {
-            carCvcIn.classList.add('shake-input');
-            carCvcIn.style = `
-            border: 1px solid red; ` 
-            let wrn = document.querySelector('#wrnName');
-            wrnCvc.innerHTML='Wrong format';
+
+        if (!regex.test(value)) {
+          setError(input, 'Only specific characters are allowed');
+        }
+
+        if (value.length > maxLength) {
+          setError(input, `Maximum ${maxLength} characters allowed`);
+        }
+
+        if (value.length > 0 && regex.test(value) && value.length <= maxLength) {
+          setSuccess(input);
+          if (display1 == carNum){
+            display1.innerHTML = value.replace(/(\d{4})/g, '$1 ');
+          }
+          else{
+            display1.innerHTML= value;
+    
+          }
 
         }
-    });
+      });
 }
-function conFirm(){
 
+
+const fields = [
+    { input: username, regex: regex_char, maxLength: 25, message: 'Card name',display1: carName  },
+    { input: card_num, regex: regex_num, maxLength: 16, message: 'Card number',display1: carNum  },
+    { input: cardMm, regex: regex_num, maxLength: 2, message: 'Month',display1: carMm  },
+    { input: cardYy, regex: regex_num, maxLength: 2, message: 'Year',display1: carYy  },
+    { input: cardCvc, regex: regex_num, maxLength: 3, message: 'CVC',display1: carCvc  },
+  ];
+  fields.forEach(({ input, regex, maxLength,display1 }) => {
+    liveUpinfo(input, regex, maxLength, display1);
+  });
+
+
+
+function validateInputs() {
+
+  fields.forEach(({ input, regex, maxLength, message }) => {
+    const value = input.value.trim();
+
+    if (value === '') {
+      setError(input, `${message} is empty`);
+    } else if (!regex.test(value)) {
+      setError(input, `Only numbers are allowed for ${message}`);
+    } else if (value.length > maxLength) {
+      setError(input, `Only ${maxLength} numbers are allowed for ${message}`);
+    } else {
+      setSuccess(input);
+      document.querySelector('.compsub').style=`display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      padding: 20px;
+      `;
+      document.querySelector('.detail').style.display= 'none';
+    }
+  });
+
+}
+function setError(element, message) {
+  const inputGroup = element.parentElement;
+  const errorElement = inputGroup.querySelector('.error');
+  inputGroup.classList.add('error1');
+  inputGroup.classList.remove('success');
+  errorElement.innerText = message;
+}
+function setSuccess(element) {
+  const inputGroup = element.parentElement;
+  const errorElement = inputGroup.querySelector('.error');
+  inputGroup.classList.remove('error1');
+  inputGroup.classList.add('success');
+  errorElement.innerText = '';
 }
